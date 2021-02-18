8ce5644286f380e9f29e230e4a50850cf059d083
Catherine <c.muthoni@alustudent.com>
2021-02-18 22:03:52 +0200

    commit4

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
+include('../elsatech/css/styles.css');
 include('index.html');
 include('aboutus.html');
 include('contact.html');
 include('products.html');
-include('../elsatech/css/styles.css');
 ?>
